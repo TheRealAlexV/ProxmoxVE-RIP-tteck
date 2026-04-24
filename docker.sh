997bee7aea2c8af7e506885577eb814353558acc
TheRealAlexV <demine0@gmail.com>
2026-04-24 10:04:41 -0400

    feat(docker): add Docker setup script with update functionality
    fix(build1.func): update PVE version check to support 8.1 and later

---
+++ docker.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+source <(curl -s https://raw.githubusercontent.com/TheRealAlexV/ProxmoxVE-RIP-tteck/refs/heads/main/misc/build1.func)
+source <(curl -s https://raw.githubusercontent.com/TheRealAlexV/ProxmoxVE-RIP-tteck/refs/heads/main/misc/build2.func)
+source <(curl -s https://raw.githubusercontent.com/TheRealAlexV/ProxmoxVE-RIP-tteck/refs/heads/main/misc/build3.func)
+# Copyright (c) 2021-2025 tteck
+# Author: tteck (tteckster)
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# Source: https://www.docker.com/
+
+APP="Docker"
+var_tags="docker"
+var_cpu="2"
+var_ram="2048"
+var_disk="4"
+var_os="debian"
+var_version="12"
+var_unprivileged="1"
+
+header_info "$APP"
+variables
+color
+catch_errors
+
+function update_script() {
+    header_info
+    check_container_storage
+    check_container_resources
+    if [[ ! -d /var ]]; then
+        msg_error "No ${APP} Installation Found!"
+        exit
+    fi
+    msg_info "Updating ${APP} LXC"
+    $STD apt-get update
+    $STD apt-get -y upgrade
+    msg_ok "Updated ${APP} LXC"
+    exit
+}
+
+start
+build_container
+description
+
+msg_ok "Completed Successfully!\n"
+echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
